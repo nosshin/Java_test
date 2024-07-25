4885062e7e5d5ef9a1ca2c9324399315893b9c46
nosshin <nosshin@gmail.com>
2024-07-25 13:31:41 +0900

    temp2

--- Java_test/Hello.java
+++ Java_test/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Test ÀÚ¹Ù 123");
         System.out.println("Java java");
+        System.out.println("Git");
     }
 }
+
 
